cf19aa9c7be1993d3e27e11ab57c31b798a5696e
thnielseen <tess.prog@gmail.com>
2025-05-07 23:18:33 +0200

    chore(structure): add src folders with .keep files for components and store

---
+++ book-client/src/App.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header>
+    <!-- When header is used mount app.component in index.ts for global component -->
+  </header>
+
+  <main>
+    <RouterView />
+  </main>
+
+  <footer>
+    <!-- If footer is used mount app.component in index.ts for global component -->
+  </footer>
+</template>
+
+<style scoped></style>
