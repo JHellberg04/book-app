de06b8cf288f3dbe5e9d19113bc728b6dc74dd9c
thnielseen <tess.prog@gmail.com>
2025-05-08 01:44:39 +0200

    config: extend Vite aliases and enable global SCSS setup
    - Added SCSS path aliases for abstracts, base, components, layout and themes
    - Enabled global `@use "@abstracts" as *;` injection in all Vue components

--- book-client/vite.config.ts
+++ book-client/vite.config.ts
@@ -7,7 +7,19 @@ export default defineConfig({
   plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      '@abstracts': fileURLToPath(new URL('./src/scss/abstracts', import.meta.url)),
+      '@base': fileURLToPath(new URL('./src/scss/base', import.meta.url)),
+      '@components': fileURLToPath(new URL('./src/scss/components', import.meta.url)),
+      '@layout': fileURLToPath(new URL('./src/scss/layout', import.meta.url)),
+      '@themes': fileURLToPath(new URL('./src/scss/themes', import.meta.url)),
+    }
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@use "@abstracts" as *;`
+      }
     }
   }
 })
