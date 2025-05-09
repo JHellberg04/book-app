b3d2e9c953094cfb93c58822a14a8b93d92c104c
Alexandra <al3xandra92@hotmail.com>
2025-05-09 09:33:39 +0200

    feat: add BookInfo component to display book details

---
+++ book-client/src/components/reviews/BookInfo.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+defineProps<{
+  title: string
+  author: string
+  published: string
+  rating: number
+  description: string
+}>()
+</script>
+
+<template>
+  <div class="book-info">
+    <h1>{{ title }}</h1>
+    <p>Author: {{ author }}</p>
+    <p>Published: {{ published }}</p>
+    <p>Rating: {{ rating }}/5</p>
+    <p>{{ description }}</p>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.book-info {
+  margin: 0;
+}
+</style>
