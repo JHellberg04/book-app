10843054fbf70f0e35366a8c0d81c6b8ab7fd070
Jonatan Hellberg <Jonatan.Hellberg@medieinstitutet.se>
2025-05-11 00:47:38 +0200

    feat: Display all books seperately

---
+++ book-client/src/components/books/BookCard.vue
@@ -0,0 +1,32 @@
+<template>
+  <article class="book-card">
+    <img :src="book.image" :alt="'Book cover for ' + book.title"/>
+
+    <div class="book-info">
+      <h2 class="book-title">{{ book.title }}</h2>
+      <p class="book-author">By {{ book.author }}</p>
+      <p class="book-year">{{ book.published_year }}</p>
+      <p class="book-genres">
+        <span v-for="genre in book.genres" :key="genre" class="genre">{{ genre }}</span>
+      </p>
+      <router-link :to="`/bookshelf/${book._id}`" class="book-link">Read more</router-link>
+    </div>
+  </article>
+</template>
+
+<script setup lang="ts">
+import { defineProps } from 'vue';
+
+interface Book {
+  _id: string;
+  title: string;
+  author: string;
+  published_year: number;
+  genres: string[];
+  image: string;
+}
+
+const props = defineProps<{
+  book: Book;
+}>();
+</script>
