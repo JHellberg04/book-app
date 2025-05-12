81c78a4cac37e0ab08a5ee8e65ba6837010e7817
thnielseen <tess.prog@gmail.com>
2025-05-12 20:50:03 +0200

    style(bookheader): rename BEM-structure and apply rem-based styles with mixin
    Co-authored-by: Namn Efternamn <al3xandra92@hotmail.com>

--- book-client/src/components/reviews/BookHeader.vue
+++ book-client/src/components/reviews/BookHeader.vue
@@ -1,29 +1,27 @@
-<template>
-  <div class="book-header">
-    <button @click="$router.back()">⬅</button>
-    <img :src="image" alt="Book Cover" class="book-cover" />
-  </div>
-</template>
-
 <script setup lang="ts">
 const props = defineProps<{
   image: string
 }>()
 </script>
 
+<template>
+  <div class="bookheader">
+    <button @click="$router.back()">⬅</button>
+    <img :src="image" alt="Book Cover" class="bookheader__cover" />
+  </div>
+</template>
+
 <style scoped lang="scss">
 .book-header {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
+  @include mix-flex-center();
 
-.book-cover {
-  width: 100%;
-  max-width: 600px;
-  height: auto;
-  max-height: 600px;
-  object-fit: contain;
-  border-radius: 8px;
+  &__cover {
+    width: 100%;
+    max-width: fn-rem(600);
+    height: auto;
+    max-height: fn-rem(600);
+    object-fit: contain;
+    border-radius: fn-rem(8);
+  }
 }
 </style>
