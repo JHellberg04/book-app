9ddbe5b13111a4a1cb5582fdb6ee2812f8892283
thnielseen <tess.prog@gmail.com>
2025-05-12 13:22:22 +0200

    feat: add listinfo prop and rem-based responsive styles to ListItem

---
+++ book-client/src/components/auth/MoleculeListItem.vue
@@ -0,0 +1,49 @@
+<script setup lang="ts">
+import AtomList from '@/components/auth/AtomListIcon.vue'
+
+const props = defineProps<{
+  listinfo: string
+}>()
+</script>
+
+<template>
+  <li class="listitem">
+    <AtomList class="listitem__icon" />
+    <span class="list-item__text">{{ listinfo }}</span>
+  </li>
+</template>
+
+<style scoped lang="scss">
+.listitem {
+  display: flex;
+  align-items: center;
+  gap: fn-rem(8); // 0.5rem = 8px
+
+  &__text {
+    font-size: fn-rem(16);
+
+    @include mix-media(tablet) {
+      font-size: fn-rem(18);
+    }
+
+    @include mix-media(laptop) {
+      font-size: fn-rem(20);
+    }
+  }
+
+  &__icon {
+    width: fn-rem(24);
+    height: fn-rem(24);
+
+    @include mix-media(tablet) {
+      width: fn-rem(26);
+      height: fn-rem(26);
+    }
+
+    @include mix-media(laptop) {
+      width: fn-rem(28);
+      height: fn-rem(28);
+    }
+  }
+}
+</style>
