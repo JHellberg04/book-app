168e3ce6b0f4237c23ac34fffa91112457eb53e5
thnielseen <tess.prog@gmail.com>
2025-05-12 22:31:14 +0200

    feat: implement HomeView using HeroLogo, HomeInfo and AccountCTA

--- book-client/src/views/HomeView.vue
+++ book-client/src/views/HomeView.vue
@@ -1,8 +1,20 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import LandingCTA from '@/components/auth/AtomButtonCta.vue'
+import AtomHeroLogo from '@/components/auth/AtomHeroLogo.vue'
+import OragnismHomeInfo from '@/components/auth/OragnismHomeInfo.vue'
+</script>
 
 <template>
   <section class="home">
-    <h1>HomeSection</h1>
+    <h1 class='home__title'>Welcome to Peach Pages</h1>
+    <AtomHeroLogo />
+    <OragnismHomeInfo />
+    <LandingCTA
+      class="home__cta"
+      heading="Ready to start building your shelf?"
+      to="/account/register"
+      label="Create Account"
+    />
   </section>
 </template>
 
@@ -10,5 +22,10 @@
 .home {
   width: 100%;
   height: 100%;
+  @include mix-flex-center(column);
+
+  &__title{
+    @include mix-flex-center();
+  }
 }
 </style>
