fe0757e2aaf1928665e07c808d6a00740f3026e2
thnielseen <tess.prog@gmail.com>
2025-05-14 07:05:53 +0200

    feat: display formatted user list using BaseTable component and add temporary to homesection

--- book-client/src/views/HomeView.vue
+++ book-client/src/views/HomeView.vue
@@ -1,8 +1,12 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import UsersTable from '@/components/admin/UsersTable.vue'
+</script>
 
 <template>
   <section class="home">
     <h1>HomeSection</h1>
+
+    <UsersTable />
   </section>
 </template>
 
